5cee481c02ed302e3363ad061faacb938f6e550e
Nico Proto <nicolasproto@gmail.com>
2021-03-02 11:13:28 +0100

    Finished lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  # See all restaurants
-  get "restaurants", to: "restaurants#index"
-  # See details about one restaurant
-  get "restaurants/:id", to: "restaurants#show"
+  # # See all restaurants ✅
+  # get "restaurants", to: "restaurants#index"
 
-  # Create a restaurant
-  # Update a restaurant
-  # Destroy a restaurant
+  # # Show the restaurant form
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # # Create a restaurant
+  # post "restaurants", to: "restaurants#create"
+
+  # # See details about one restaurant
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # # Update a restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # # Destroy a restaurant
+  # delete "restaurants/:id", to: "restaurants#destroy"
+
+  resources :restaurants
 end
