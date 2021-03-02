faea0ae2a4b8623e13b8de981136686fb5e35f66
Nico Proto <nicolasproto@gmail.com>
2021-03-02 09:37:22 +0100

    Added index for restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # See all restaurants
+  get "restaurants", to: "restaurants#index"
+  # See details about one restaurant
+  get "restaurants/:id", to: "restaurants#show"
+
+  # Create a restaurant
+  # Update a restaurant
+  # Destroy a restaurant
 end
